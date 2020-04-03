1da3dcce1d78c4675df69f580a82f21c4a4c6dff
Isaac Mecchi <isaacmecchi@gmail.com>
2020-04-03 15:39:08 -0300

    feature #2 testando RestTemplate no PedidosServiceController.

---
+++ src/main/java/br/com/casadocodigo/loja/models/Pedidos.java
@@ -0,0 +1,50 @@
+package br.com.casadocodigo.loja.models;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Pedidos {
+
+	private int id;
+
+	private BigDecimal valor;
+
+	private Calendar data;
+
+	private List<Produto> produtos = new ArrayList<>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	};
+
+}
