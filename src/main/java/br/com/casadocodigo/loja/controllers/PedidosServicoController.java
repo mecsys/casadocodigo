72f460477156d7a575a72a593b98a5553810c9ed
Isaac Mecchi <isaacmecchi@gmail.com>
2020-04-03 12:15:35 -0300

    Usando RestTemplate no PedidoServiceController.

--- src/main/java/br/com/casadocodigo/loja/controllers/PedidosServicoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/PedidosServicoController.java
@@ -2,14 +2,28 @@ package br.com.casadocodigo.loja.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.casadocodigo.loja.models.Produto;
 
 @Controller
 public class PedidosServicoController {
+	
+	private RestTemplate restTemplate;
 
 	@RequestMapping("/pedidos")
-	public String listarPedidos( ) {
+	public ModelAndView listarPedidos(RedirectAttributes model) {
 		
-		return "URL /pedidos executada";
+		String uri = "https://book-payment.herokuapp.com/orders";
+		String response = restTemplate.getForObject(uri, String.class);
+		
+		System.out.println(response);
+		
+		model.addFlashAttribute("sucesso", "Lista de Produtos Obtida com Sucesso");
+		
+		return new ModelAndView("redirect:pedidos");
 	}
 	
 }
