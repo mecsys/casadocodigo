6b572ee01fce55d69bd9072cbbc15c83298de472
Isaac Mecchi <isaacmecchi@gmail.com>
2020-04-04 16:38:37 -0300

    feature #3 ajustando e validando cadastrar usuarios.

--- src/main/java/br/com/casadocodigo/loja/controllers/UserController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/UserController.java
@@ -5,19 +5,18 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.dao.UsuarioDAO;
-import br.com.casadocodigo.loja.models.Produto;
-import br.com.casadocodigo.loja.models.TipoPreco;
 import br.com.casadocodigo.loja.models.Usuario;
+import br.com.casadocodigo.loja.validation.UsuarioValidation;
 
 @Controller
 public class UserController {
@@ -29,6 +28,11 @@ public class UserController {
 	
 	@Autowired 
 	private Usuario usuario;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new UsuarioValidation());
+	}
 	
 	@RequestMapping("/usuarios")
 	public ModelAndView listarUsuarios(RedirectAttributes model) {
