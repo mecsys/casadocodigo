e1f013f6b0425512583fc2468bbd920affae0a77
Isaac Mecchi <isaacmecchi@gmail.com>
2020-04-04 17:03:36 -0300

    feature #3 ajustando e validando cadastrar usuarios.

--- src/main/java/br/com/casadocodigo/loja/controllers/UserController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/UserController.java
@@ -2,14 +2,21 @@ package br.com.casadocodigo.loja.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.dao.UsuarioDAO;
 import br.com.casadocodigo.loja.models.Usuario;
+import br.com.casadocodigo.loja.validation.UsuarioValidation;
 
 @Controller
 public class UserController {
@@ -19,10 +26,10 @@ public class UserController {
 	@Autowired
 	private UsuarioDAO usuarioDao;
 	
-//	@InitBinder
-//	public void initBinder(WebDataBinder binder) {
-//		binder.addValidators(new UsuarioValidation());
-//	}
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new UsuarioValidation());
+	}
 	
 	@RequestMapping("/usuarios")
 	public ModelAndView listarUsuarios(RedirectAttributes model) {
@@ -38,27 +45,27 @@ public class UserController {
 		
 	}
 	
-//	@RequestMapping("/usuario/form")
-//	public ModelAndView form(Usuario usuario) {
-//		
-//		return new ModelAndView("produtos/form");
-//		
-//	}
-//	
-//	@RequestMapping(method = RequestMethod.POST)
-//	public ModelAndView gravar(@Valid Usuario usuario, BindingResult result, 
-//			RedirectAttributes model) {
-//		
-//		if(result.hasErrors()) {
-//			return form(usuario);
-//		}
-//		
-//		usuarioDao.gravar(usuario);
-//		
-//		model.addFlashAttribute("sucesso", "Usuario cadastrado com sucesso!");
-//		
-//		return new ModelAndView("usuarios/lista");
-//		
-//	}
+	@RequestMapping("/usuario/form")
+	public ModelAndView form(Usuario usuario) {
+		
+		return new ModelAndView("produtos/form");
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView gravar(@Valid Usuario usuario, BindingResult result, 
+			RedirectAttributes model) {
+		
+		if(result.hasErrors()) {
+			return form(usuario);
+		}
+		
+		usuarioDao.gravar(usuario);
+		
+		model.addFlashAttribute("sucesso", "Usuario cadastrado com sucesso!");
+		
+		return new ModelAndView("usuarios/lista");
+		
+	}
 	
 }
