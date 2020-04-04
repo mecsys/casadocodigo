45ee1fcfcbe68a18f39886c7baa5d4d2248c6e32
Isaac Mecchi <isaacmecchi@gmail.com>
2020-04-04 16:53:20 -0300

    feature #3 ajustando e validando cadastrar usuarios.

--- src/main/java/br/com/casadocodigo/loja/controllers/UserController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/UserController.java
@@ -2,21 +2,14 @@ package br.com.casadocodigo.loja.controllers;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.dao.UsuarioDAO;
 import br.com.casadocodigo.loja.models.Usuario;
-import br.com.casadocodigo.loja.validation.UsuarioValidation;
 
 @Controller
 public class UserController {
