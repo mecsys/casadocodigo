8d589408080ae29d868696704b56aa392683756f
Isaac Mecchi <isaacmecchi@gmail.com>
2020-04-04 23:54:11 -0300

    feature #3 ajustando e validando cadastrar usuarios.

--- src/main/java/br/com/casadocodigo/loja/dao/UsuarioDAO.java
+++ src/main/java/br/com/casadocodigo/loja/dao/UsuarioDAO.java
@@ -8,11 +8,13 @@ import javax.persistence.PersistenceContext;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.Usuario;
 
 @Repository
+@Transactional
 public class UsuarioDAO implements UserDetailsService{
 
 	@PersistenceContext
