2ad0ef215cc6904c02b2e60512a6f552d342d87e
Isaac Mecchi <isaacmecchi@gmail.com>
2020-04-07 01:16:34 -0300

    feature #4 finalizada. ativando testes do maven.

--- src/test/java/br/com/casadocodigo/loja/conf/DataSourceConfigurationTest.java
+++ src/test/java/br/com/casadocodigo/loja/conf/DataSourceConfigurationTest.java
@@ -1,6 +1,8 @@
 package br.com.casadocodigo.loja.conf;
 
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
@@ -13,11 +15,23 @@ public class DataSourceConfigurationTest {
 	@Profile("test")
 	public DataSource dataSource(){
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo_test");
+		dataSource.setUrl("jdbc:mysql://mysql-test:3306/casadocodigo_test");
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUsername("root");
-		dataSource.setPassword("");
+		dataSource.setUsername("casadocodigo_test");
+		dataSource.setPassword("casadocodigo_test");
 		return dataSource;
+	}
+	
+	@Bean
+	@Profile("test")
+	public Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.hbm2ddl.auto", "create");
+		//propriedade para mostra o DDL gerado como log
+		properties.setProperty("javax.persistence.schema-generation.scripts.create-target", "db-schema.jpa.ddl");
+		return properties;
 	}
 
 }
